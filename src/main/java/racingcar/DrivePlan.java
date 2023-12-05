e3cc9cc6d5b7752f81c29b4a5bf8e84b9e0754ff
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-06 00:03:39 +0900

    feat(DrivePlan): pick winners of race

--- src/main/java/racingcar/DrivePlan.java
+++ src/main/java/racingcar/DrivePlan.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import racingcar.valueholder.CarName;
 import racingcar.valueholder.CarNames;
@@ -23,6 +25,29 @@ public class DrivePlan {
 
     private boolean isDriving(CarName carName, RaceLap raceLap) {
         return drivePlan.get(carName).getTrial(raceLap) >= Settings.STANDARD_TO_DRIVE.getNumber();
+    }
+
+    public CarNames pickWinners() {
+        var driveLog = createDriveLog();
+        int maxDrive = computeMaxDrive(driveLog);
+
+        return new CarNames(driveLog.entrySet().stream()
+                .filter(entry -> entry.getValue() == maxDrive)
+                .map(Map.Entry::getKey)
+                .toList());
+    }
+
+    private Map<CarName, Integer> createDriveLog() {
+        return drivePlan.entrySet().stream().collect(Collectors.toMap(
+                Map.Entry::getKey,
+                entry -> computeDriveByRaceLap(entry.getKey(), new RaceLap(getMaxRaceLap().number())),
+                (oldValue, newValue) -> oldValue,
+                LinkedHashMap::new
+        ));
+    }
+
+    private int computeMaxDrive(Map<CarName, Integer> driveLog) {
+        return driveLog.values().stream().max(Integer::compareTo).orElse(0);
     }
 
     public MaxRaceLap getMaxRaceLap() {
