a901a0602f695244c697121bb43db5dd2d735578
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-06 00:39:38 +0900

    refactor(PaceMaker): split to DriveTrialsGenerator for better modularity

---
+++ src/main/java/racingcar/DriveTrialsGenerator.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static racingcar.Settings.MAXIMUM_DRIVE_TRIAL;
+import static racingcar.Settings.MINIMUM_DRIVE_TRIAL;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import racingcar.valueholder.MaxRaceLap;
+
+public class DriveTrialsGenerator {
+    public static List<Integer> generateDriveTrials(MaxRaceLap maxRaceLap) {
+        return IntStream.range(0, maxRaceLap.number())
+                .mapToObj(i -> pickNumberInRange(MINIMUM_DRIVE_TRIAL.getNumber(), MAXIMUM_DRIVE_TRIAL.getNumber()))
+                .toList();
+    }
+}
--- src/main/java/racingcar/PaceMaker.java
+++ src/main/java/racingcar/PaceMaker.java
@@ -1,38 +1,20 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-import static racingcar.Settings.MAXIMUM_DRIVE_TRIAL;
-import static racingcar.Settings.MINIMUM_DRIVE_TRIAL;
+import static racingcar.DriveTrialsGenerator.generateDriveTrials;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import racingcar.valueholder.CarName;
 import racingcar.valueholder.CarNames;
 import racingcar.valueholder.DriveTrials;
 import racingcar.valueholder.MaxRaceLap;
 
 public class PaceMaker {
-    private final List<CarName> carNames;
-    private final int maxRaceLap;
 
-    public PaceMaker(final CarNames carNames, final MaxRaceLap maxRaceLap) {
-        this.carNames = carNames.names();
-        this.maxRaceLap = maxRaceLap.number();
-    }
-
-    public DrivePlan createDrivePlan() {
-        return new DrivePlan(carNames.stream()
-                .collect(Collectors.toMap(Function.identity(), key -> new DriveTrials(generateDriveTrials()),
+    public static DrivePlan createDrivePlan(CarNames carNames, MaxRaceLap maxRaceLap) {
+        return new DrivePlan(carNames.names().stream()
+                .collect(Collectors.toMap(Function.identity(), key -> new DriveTrials(generateDriveTrials(maxRaceLap)),
                         (oldValue, newValue) -> oldValue,
                         LinkedHashMap::new)));
-    }
-
-    private List<Integer> generateDriveTrials() {
-        return IntStream.range(0, maxRaceLap)
-                .mapToObj(i -> pickNumberInRange(MINIMUM_DRIVE_TRIAL.getNumber(), MAXIMUM_DRIVE_TRIAL.getNumber()))
-                .toList();
     }
 }
