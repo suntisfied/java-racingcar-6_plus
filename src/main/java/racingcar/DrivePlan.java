33d104fee8cab4b4f899823f7a07d91193090830
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-05 23:27:34 +0900

    feat(DrivePlan): compute drive amount by race lap

--- src/main/java/racingcar/DrivePlan.java
+++ src/main/java/racingcar/DrivePlan.java
@@ -1,9 +1,11 @@
 package racingcar;
 
 import java.util.Map;
+import java.util.stream.IntStream;
 import racingcar.valueholder.CarName;
 import racingcar.valueholder.CarNames;
 import racingcar.valueholder.DriveTrials;
+import racingcar.valueholder.MaxRaceLap;
 import racingcar.valueholder.RaceLap;
 
 public class DrivePlan {
@@ -13,11 +15,21 @@ public class DrivePlan {
         this.drivePlan = drivePlan;
     }
 
-    public CarNames getCarNames() {
-        return new CarNames(drivePlan.keySet().stream().toList());
+    public int computeDriveByRaceLap(CarName carName, RaceLap raceLap) {
+        return (int) IntStream.range(0, raceLap.number())
+                .filter(currentLap -> isDriving(carName, new RaceLap(currentLap)))
+                .count();
     }
 
-    public boolean isDriving(CarName carName, RaceLap raceLap) {
+    private boolean isDriving(CarName carName, RaceLap raceLap) {
         return drivePlan.get(carName).getTrial(raceLap) >= Settings.STANDARD_TO_DRIVE.getNumber();
+    }
+
+    public MaxRaceLap getMaxRaceLap() {
+        return new MaxRaceLap(drivePlan.values().stream().toList().get(0).trials().size());
+    }
+
+    public CarNames getCarNames() {
+        return new CarNames(drivePlan.keySet().stream().toList());
     }
 }
