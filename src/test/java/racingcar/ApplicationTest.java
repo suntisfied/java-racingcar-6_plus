b28bf469ed735868e8ec538cbef5b213b17d6d13
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-06 19:20:50 +0900

    fix(ApplicationTest): ensure to reflect the change of requirement
    - Updated the ApplicationTest to handle scenarios where invalid input is provided.
    - The test now checks for an error message and verifies that the system prompts for input again until a correct input is entered.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,16 +1,16 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
     void 전진_정지() {
@@ -25,10 +25,10 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 이름에_대한_예외_처리() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> {
+            runException("pobi,javaji", "1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Override
