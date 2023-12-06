17abf1a9ff937b581310950a89c371ccb166f200
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-06 15:35:20 +0900

    feat(RaceLapValidator): throw exception to invalid input

---
+++ src/main/java/validator/RaceLapValidator.java
@@ -0,0 +1,17 @@
+package validator;
+
+import static racingcar.util.ValidatingUtils.isPositiveInteger;
+
+import racingcar.inputform.InputValidatorForm;
+import racingcar.setting.Texts;
+
+public class RaceLapValidator extends InputValidatorForm {
+    public static void checkRaceLapValidity(String input) {
+        throwIllegalArgumentExceptionToInvalid(Texts.ERROR_INVALID_RACE_LAP.text,
+                () -> validateRaceLap(input));
+    }
+
+    private static boolean validateRaceLap(String input) {
+        return isPositiveInteger.test(input);
+    }
+}
