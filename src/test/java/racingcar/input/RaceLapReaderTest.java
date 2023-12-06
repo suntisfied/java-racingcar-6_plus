9fba6575ab0568d5b523bba5da995218378f2b69
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-06 15:36:22 +0900

    test(RaceLapValidator): ensure to throw exception to invalid input

---
+++ src/test/java/racingcar/input/RaceLapReaderTest.java
@@ -0,0 +1,25 @@
+package racingcar.input;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+class RaceLapReaderTest {
+    @ParameterizedTest
+    @CsvSource({
+            "a, 3",
+    })
+    void readValidInputAfterInvalidOne(String invalidInput, String validInput) {
+        try (MockedStatic<Console> mockConsole = Mockito.mockStatic(Console.class)) {
+            mockConsole.when(Console::readLine).thenReturn(invalidInput, validInput);
+
+            String actualInput = RaceLapReader.readRaceLap();
+
+            assertEquals(validInput, actualInput);
+        }
+    }
+}
