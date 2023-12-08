be63fbbdadbabb6212c30700e568741b180b9ab2
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-12-08 12:11:17 +0900

    refactor(DriveTrials): simplify dependency injection process for initialization

--- src/main/java/racingcar/valueholder/DriveTrials.java
+++
@@ -1,9 +0,0 @@
-package racingcar.valueholder;
-
-import java.util.List;
-
-public record DriveTrials(List<Integer> trials) {
-    public int getTrial(RaceLap raceLap) {
-        return trials.get(raceLap.number());
-    }
-}
